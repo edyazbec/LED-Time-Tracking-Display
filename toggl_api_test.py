2e16aa21d0024ebc152cac4ae79b8be8bc769022
edyazbec <70415393+edyazbec@users.noreply.github.com>
2022-08-13 14:19:07 -0400

    API Test to retrieve json data

---
+++ toggl_api_test.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import datetime
+
+if __name__ == '__main__':
+	#get the current day and figure out the most recent monday
+	today = datetime.date.today()
+	lastMonday = datetime.date.today() - datetime.timedelta(days=today.weekday())
+	#parameters for the api call
+	url='https://api.track.toggl.com/reports/api/v2/weekly'
+	api_key=''#enter your api key from the website
+	params={'user_agent':'python_api_test','workspace_id':'','since':lastMonday} #enter your workspace id
+	#make the api call
+	response=requests.get(url,auth=(api_key,'api_token'),params=params)
+	response.json=json.loads(response.text)
+	#create / update the projects
+	projects=[]
+	if(response.ok):
+		with open('togglData.json', 'w') as f:
+			json.dump(response.json, f)
